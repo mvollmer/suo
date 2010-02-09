dedadab75d016ef0b26bac73ab5ba598922ef115
Marius Vollmer <mvo@bright.(none)>
2010-02-09 21:27:25 +0200

    Starting over.

--- suo-null-compiler.scm
+++
@@ -1,7 +0,0 @@
-;;; A do-nothing compiler
-
-;; This is used to quickly test changes to the bootstrapping process
-;; when bootstrapping the real compiler would be too slow.
-
-(define (compile exp)
-  (macroexpand exp))
